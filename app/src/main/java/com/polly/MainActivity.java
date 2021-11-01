057023f5d7128da19a8b653fd91ae486f1d31ed9
BuildTools <unconfigured@null.spigotmc.org>
2021-11-01 14:12:46 +0100

    Made the following changes: - set content view to activity_main - added the options menu - created actions that are performed when selecting a menu item (TODO)

--- app/src/main/java/com/polly/MainActivity.java
+++ app/src/main/java/com/polly/MainActivity.java
@@ -1,9 +1,41 @@
 package com.polly;
 
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    public MainActivity(){
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        switch(id){
+            case R.id.menu_main_settings:
+                //TODO: zu settings-seite wechseln
+                return true;
+            case R.id.activity_login_button_sign_up:
+                //TODO: zu login-seite wechseln
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
