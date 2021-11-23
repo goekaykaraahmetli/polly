533efbb35bad026a65560e4b35c972cbd9bb2062
BuildTools <unconfigured@null.spigotmc.org>
2021-11-23 20:17:03 +0100

    changed hardcoded ip address to dynamic dns

--- app/src/main/java/com/polly/config/Config.java
+++ app/src/main/java/com/polly/config/Config.java
@@ -1,7 +1,7 @@
 package com.polly.config;
 
 public class Config {
-	public static final String SERVER_IP_ADRESS = "87.155.246.172";
+	public static final String SERVER_IP_ADRESS = "pollyserver.ddns.net";
 	public static final int SERVER_PORT = 1337;
 	public static final int DATA_STREAM_MANAGER_REFRESH_DELAY = 20;
 
