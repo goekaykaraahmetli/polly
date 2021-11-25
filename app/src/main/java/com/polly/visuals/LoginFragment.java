cb417b184c60ddde183e33c215d7d2317f7659e1
moritz.willimowski <moritz.willimowski@stud.tu-darmstadt.de>
2021-11-25 15:30:11 +0100

    google login

--- app/src/main/java/com/polly/visuals/LoginFragment.java
+++ app/src/main/java/com/polly/visuals/LoginFragment.java
@@ -68,7 +68,7 @@ public class LoginFragment extends Fragment {
         mAuth = FirebaseAuth.getInstance();
 
 
-        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken("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").requestEmail().build();
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build();
         googleSignInClient = GoogleSignIn.getClient(getActivity(),googleSignInOptions);
         ImageButton googleBtn = (ImageButton) view.findViewById(R.id.imageButtonGoogle);
         googleBtn.setOnClickListener(v -> {
