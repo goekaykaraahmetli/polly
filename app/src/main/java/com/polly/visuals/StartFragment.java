daab74ff86d71d20b75befa8880b937e50812338
willi <moritz.willimowski@stud.tu-darmstadt.de>
2021-12-02 12:37:59 +0100

    fixed alot of things

---
+++ app/src/main/java/com/polly/visuals/StartFragment.java
@@ -0,0 +1,23 @@
+package com.polly.visuals;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import com.polly.R;
+
+public class StartFragment extends Fragment {
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        View root = inflater.inflate(R.layout.fragment_start, container, false);
+        return root;
+    }
+}
