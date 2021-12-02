6a3cf78101900e4612758adbbbe3c10b1f02299e
BuildTools <unconfigured@null.spigotmc.org>
2021-12-02 13:23:43 +0100

    changed SignupActivity.java to SignupFragment.java

--- app/src/main/java/com/polly/visuals/AccountFragment.java
+++ app/src/main/java/com/polly/visuals/AccountFragment.java
@@ -60,6 +60,7 @@ public class AccountFragment extends Fragment {
                     GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build();
                     GoogleSignInClient googleSignInClient = GoogleSignIn.getClient(getActivity(), googleSignInOptions);
                     googleSignInClient.signOut();
+                    view.findViewById(R.id.logout_button).setVisibility(View.INVISIBLE);
                 }
             }
         });
