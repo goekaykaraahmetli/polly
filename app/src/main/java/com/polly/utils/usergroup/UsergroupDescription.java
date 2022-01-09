459f6a3c9f41936c585051558a9732f9258461f4
BuildTools <unconfigured@null.spigotmc.org>
2022-01-09 17:08:29 +0100

    added UsergroupDescription

---
+++ app/src/main/java/com/polly/utils/usergroup/UsergroupDescription.java
@@ -0,0 +1,13 @@
+package com.polly.utils.usergroup;
+
+public class UsergroupDescription {
+	private final String usergroupDescriptionText;
+
+	public UsergroupDescription(String description) {
+		usergroupDescriptionText = description;
+	}
+
+	public String getDescription() {
+		return usergroupDescriptionText;
+	}
+}
