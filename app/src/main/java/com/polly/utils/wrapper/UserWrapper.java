593dd8e771ebbacb8d89dab46582fcc3df7ab711
BuildTools <unconfigured@null.spigotmc.org>
2022-01-09 16:57:24 +0100

    added GetMyUsergroupsCommand

---
+++ app/src/main/java/com/polly/utils/wrapper/UserWrapper.java
@@ -0,0 +1,13 @@
+package com.polly.utils.wrapper;
+
+public class UserWrapper {
+	private final String name;
+	
+	public UserWrapper(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
