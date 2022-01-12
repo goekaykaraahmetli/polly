9bd84b1f0880090c491f9f863ee9ba9c3310816c
BuildTools <unconfigured@null.spigotmc.org>
2022-01-12 16:44:12 +0100

    added chooseArea

--- app/src/main/java/com/polly/visuals/ChooseAreaFragment.java
+++ app/src/main/java/com/polly/visuals/ChooseAreaFragment.java
@@ -4,25 +4,48 @@ import android.Manifest;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Looper;
 import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.PermissionToken;
 import com.karumi.dexter.listener.PermissionDeniedResponse;
@@ -31,10 +54,18 @@ import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.single.PermissionListener;
 import com.polly.R;
 import com.polly.utils.Area;
+import com.polly.utils.Location;
 
 public class ChooseAreaFragment extends Fragment implements OnMapReadyCallback {
     private boolean locationPermissionGranted;
     private GoogleMap googleMap;
+    private LocationRequest locationRequest;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private SeekBar seekBar;
+    private TextView textView;
+
+    private LatLng markerLocation;
+
 
     @Nullable
     @Override
@@ -42,17 +73,30 @@ public class ChooseAreaFragment extends Fragment implements OnMapReadyCallback {
                              @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_choosearea, container, false);
 
+        seekBar = root.findViewById(R.id.mapSeekBar);
+        textView = root.findViewById(R.id.mapTextView);
+        Button button = root.findViewById(R.id.mapButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(markerLocation != null) {
+                    saveArea();
+                } else {
+                    Toast.makeText(getContext(), "Please Select A Location First!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
-
-        while(!locationPermissionGranted)
+        while (!locationPermissionGranted)
             checkLocationPermission();
-        if(!checkGooglePlayServices()) {
+        if (!checkGooglePlayServices()) {
             Toast.makeText(getContext(), "No Google Play Services Available!", Toast.LENGTH_SHORT).show();
             return root;
         }
 
         SupportMapFragment supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.fragmentMap);
         supportMapFragment.getMapAsync(this);
+        checkGps();
 
         return root;
     }
@@ -60,9 +104,9 @@ public class ChooseAreaFragment extends Fragment implements OnMapReadyCallback {
     private boolean checkGooglePlayServices() {
         GoogleApiAvailability googleApiAvailability = GoogleApiAvailability.getInstance();
         int result = googleApiAvailability.isGooglePlayServicesAvailable(getContext());
-        if(result == ConnectionResult.SUCCESS)
+        if (result == ConnectionResult.SUCCESS)
             return true;
-        else if(googleApiAvailability.isUserResolvableError(result)) {
+        else if (googleApiAvailability.isUserResolvableError(result)) {
             Dialog dialog = googleApiAvailability.getErrorDialog(getActivity(), result, 201, new DialogInterface.OnCancelListener() {
                 @Override
                 public void onCancel(DialogInterface dialog) {
@@ -101,22 +145,124 @@ public class ChooseAreaFragment extends Fragment implements OnMapReadyCallback {
     public void onMapReady(@NonNull GoogleMap googleMap) {
         this.googleMap = googleMap;
 
+        initMap();
+    }
+
+    private void initMap() {
+        googleMap.getUiSettings().setZoomControlsEnabled(true);
+        googleMap.getUiSettings().setCompassEnabled(true);
+        googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(getContext(), "Please grant permission to user your locaiton!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        googleMap.setMyLocationEnabled(true);
+
+        moveCameraToCurrentLocation();
+
         googleMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
             @Override
             public void onMapLongClick(@NonNull LatLng latLng) {
-                MarkerOptions markerOptions = new MarkerOptions();
-                markerOptions.title("Choosen Position");
-                markerOptions.position(latLng);
-                googleMap.addMarker(markerOptions);
+                markerLocation = latLng;
+                showArea();
             }
         });
 
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                showArea();
+                textView.setText(String.valueOf(progress+1));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                showArea();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                showArea();
+            }
+        });
     }
 
+    private void checkGps() {
+        locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
 
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(locationRequest).setAlwaysShow(true);
 
-    private void saveArea(Area area) {
+        Task<LocationSettingsResponse> locationSettingsResponseTask = LocationServices.getSettingsClient(getContext()).checkLocationSettings(builder.build());
+
+        locationSettingsResponseTask.addOnCompleteListener(new OnCompleteListener<LocationSettingsResponse>() {
+            @Override
+            public void onComplete(@NonNull Task<LocationSettingsResponse> task) {
+                try {
+                    LocationSettingsResponse response = task.getResult(ApiException.class);
+                } catch (ApiException e) {
+                    if (e.getStatusCode() == LocationSettingsStatusCodes.RESOLUTION_REQUIRED) {
+                        ResolvableApiException resolvableApiException = (ResolvableApiException) e;
+
+                        try {
+                            resolvableApiException.startResolutionForResult(getActivity(), 101);
+                        } catch (IntentSender.SendIntentException sendIntentException) {
+                            sendIntentException.printStackTrace();
+                        }
+                    }
+
+                    if (e.getStatusCode() == LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE) {
+                        Toast.makeText(getContext(), "No Gps Available", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+    }
+
+    private void moveCameraToCurrentLocation() {
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, new LocationCallback() {
+            @Override
+            public void onLocationResult(@NonNull LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+
+                Location usersLocation = new Location(locationResult.getLastLocation().getLatitude(), locationResult.getLastLocation().getLongitude());
+                CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(new LatLng(usersLocation.getLatitude(), usersLocation.getLongitude()), 5);
+                googleMap.animateCamera(cameraUpdate);
+            }
+        }, Looper.myLooper());
+    }
+
+    private void saveArea() {
+        double radius = seekBar.getProgress() * 1000;
+        Area area = new Area(markerLocation.latitude, markerLocation.longitude, radius);
+
         // save area to savings
         // navigate back to create poll
+    }
+
+    private void showArea() {
+        if(markerLocation != null) {
+            googleMap.clear();
+            double radius = (seekBar.getProgress()+1) * 1000;
+
+            CircleOptions circle = new CircleOptions();
+            circle.center(markerLocation);
+            circle.radius(radius);
+            circle.strokeColor(Color.argb(255, 100, 255, 255));
+            circle.fillColor(Color.argb(100, 100, 255, 255));
+
+            MarkerOptions markerOptions = new MarkerOptions();
+            markerOptions.title("Choosen Position");
+            markerOptions.position(markerLocation);
+
+            googleMap.addMarker(markerOptions);
+            googleMap.addCircle(circle);
+        }
     }
 }
