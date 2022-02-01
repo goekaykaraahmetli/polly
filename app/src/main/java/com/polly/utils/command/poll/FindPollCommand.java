8ddfd1220e82c1b5863ba349c54dcda023fad393
BuildTools <unconfigured@null.spigotmc.org>
2022-02-01 13:14:41 +0100

    added FindPollCommand

---
+++ app/src/main/java/com/polly/utils/command/poll/FindPollCommand.java
@@ -0,0 +1,31 @@
+package com.polly.utils.command.poll;
+
+public class FindPollCommand {
+	private final String name;
+	private final String creator;
+	private final boolean active;
+	private final boolean expired;
+	
+	public FindPollCommand(String name, String creator, boolean active, boolean expired) {
+		this.name = name;
+		this.creator = creator;
+		this.active = active;
+		this.expired = expired;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getCreator() {
+		return creator;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public boolean isExpired() {
+		return expired;
+	}
+}
